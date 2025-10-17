f1c012b07c114a3cd9525ff1a2757a03bd29c934
--global <--global>
2025-10-17 17:54:23 +0600

    database start

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI
 from database import Base, engine
 from controllers.auth_controller import auth_controller
+from controllers.verification_controller import verification_controller
 from create_db import create_database
 from middlewares import cors_middleware
 from middlewares import static_middleware
@@ -20,3 +21,4 @@ Base.metadata.create_all(bind=engine)
 
 # Include routers
 app.include_router(auth_controller.router, prefix="/auth", tags=["auth"])
+app.include_router(verification_controller.router, prefix="/verification", tags=["verification"])
